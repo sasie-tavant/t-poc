6bf2927dafd811370f92f5000e39c616a3303e47
Sasindran P <sasindran.p@tavant.com>
2015-05-07 11:12:50 +0530

    validations added for teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,11 @@ class Team < ActiveRecord::Base
 
   mount_uploader :livery, ImageUploader
 
+  validates :constructor, { uniqueness: true, presence: true }
+  validates :driver1, :driver2,  { uniqueness: true, presence: true }
+  validates :livery, presence: true
+  validates :titles, { presence: true, numericality: { greater_than_or_equal_to: 0 } }
+
   belongs_to :user
 
 end
